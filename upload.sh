f190cc8104a41483dccd2430a2f2c4709ab365e8
arjit547 <97424724+arjit547@users.noreply.github.com>
2022-09-13 16:48:13 +0530

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/ubuntu
+sudo apt-get update
+sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3 -y
+sudo pip install virtualenv
+virtualenv myenv
+source myenv/bin/activate
+pip install django
