359fc96e71638c8d12920156c0ef1d0120dfd5b4
arjit547 <97424724+arjit547@users.noreply.github.com>
2022-09-13 17:45:11 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -5,4 +5,4 @@ sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3 -y
 sudo pip install virtualenv
 virtualenv myenv
 source myenv/bin/activate
-pip install django
+pip install django gunicorn
